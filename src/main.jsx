975c3a03dfe54284f969ed9511f3209be01f5727
babichsvyat-a11y <babichsvyat@gmail.com>
2026-08-15 13:29:15 +0300

    upd 1.1

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-import "./styles";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-=======
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/app";
->>>>>>> 3a835a8bf828e8cfe037ee331c4981704d7a61c7
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
-<<<<<<< HEAD
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
-  base: "/",
-=======
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 3a835a8bf828e8cfe037ee331c4981704d7a61c7
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const isProd = mode === "production";
+
+  return {
+    base: isProd ? "/todo-react/" : "/",
+    plugins: [react()],
+    build: {
+      sourcemap: true,
     },
-  },
+    base: "/",
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
 });
